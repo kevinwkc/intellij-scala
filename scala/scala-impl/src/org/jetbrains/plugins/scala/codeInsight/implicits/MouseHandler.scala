db120030c64dce0ebff7b716c4f748789398657b
Pavel Fatin <pavel.fatin@jetbrains.com>
2018-07-09 21:55:11 +0200

    Implicit hints: hide brace matching on key press, #SCL-14026

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/implicits/MouseHandler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/implicits/MouseHandler.scala
@@ -267,7 +267,11 @@ class MouseHandler(project: Project,
           highlightedMatches = matches
 
           editor.getContentComponent.addKeyListener(new KeyAdapter {
-            override def keyReleased(keyEvent: KeyEvent): Unit = {
+            override def keyPressed(keyEvent: KeyEvent): Unit = handle()
+
+            override def keyReleased(keyEvent: KeyEvent): Unit = handle()
+
+            private def handle() = {
               editor.getContentComponent.removeKeyListener(this)
               clearHighlightedMatches()
             }
